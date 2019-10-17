0b95ddb0bf0192e2b0c3cc47140e6625c3571fb7
ari <ari@mascii.org>
2019-10-17 14:24:36 -0400

    progress with .../ completion

---
+++ lib/completion.rb
@@ -0,0 +1,22 @@
+module CompletionUtils
+
+def longestCommonPrefix(choices)
+    puts "../../"
+end
+
+CompletionInput = Struct.new(:subject, :priorWord, :textSoFar, :line, :wordBreaks, :point, :kast) do 
+   def initialize()
+        #puts ENV['COMP_TYPE']
+        ## the command that triggered the completion
+        self.subject = ARGV[0]
+        self.priorWord = ARGV[1]
+        self.textSoFar = ARGV[2]
+        ## entire line including subject
+        self.line = ENV["COMP_LINE"]
+        self.wordBreaks = ENV["COMP_WORDBREAKS"]
+        ## the length of line.  equivalent to line.length.to_s
+        self.point = ENV["COMP_POINT"]
+   end 
+end
+
+end
--- tripleDotDirs.rb
+++ tripleDotDirs.rb
@@ -1,35 +1,43 @@
 #!/usr/bin/env ruby
 
+require_relative 'lib/completion.rb'
+include CompletionUtils
+
 # complete -o bashdefault -o default -C $INSTALL_DIR/minimal-bash/tripleDotDirs.rb cd
 
-#puts ENV['COMP_TYPE']
+syntax = ".../"
 
-## the command that triggered the completion
-subject = ARGV[0]
-priorword = ARGV[1]
-textsofar = ARGV[2]
-## entire line including subject
-line = ENV["COMP_LINE"]
-wordbreaks = ENV["COMP_WORDBREAKS"]
-## the length of line.  equivalent to line.length.to_s
-point = ENV["COMP_POINT"]
 
 #puts point == line.length.to_s
-levels = -1
+levels = 0 
 choices = []
 
-if ! priorword.start_with?(".../") then
+input = CompletionInput.new
+
+if ! input.priorWord.start_with?(syntax) then
     puts ""
     exit
 else
-    priorword = priorword[".../".length, priorword.length]
+    input.priorWord = input.priorWord[".../".length, input.priorWord.length]
 end
 
 Dir.pwd.sub(/^\//, '').split('/').reverse.each { |s| 
     levels +=1 
     prefix = "../" * levels
-    choices.push(Dir["#{prefix}#{priorword}*"].select {|f| 
-        File.directory? f
-    })
+    Dir["#{prefix}#{input.priorWord}*"].each{|f| 
+        if File.directory? f then
+            choices.push f
+        end
+    }
 }
-puts choices
+
+
+if choices.size <= 1 then
+    puts choices
+else
+    puts longestCommonPrefix(choices)
+end
+
+
+######### support funcs
+
