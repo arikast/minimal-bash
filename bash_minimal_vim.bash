f1bde88a7f0be422299fd45daf622c21b874fa16
ari <ari@mascii.org>
2019-10-18 13:39:14 -0400

    vim setup

--- bash_minimal_vim.bash
+++ bash_minimal_vim.bash
@@ -1,1 +1,8 @@
+## this script only needs to be run once, does NOT need to be sourced into .bashrc or .bash_profile
+
+BASH_MINIMAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+
 mkdir -p $HOME/.vim-tmp/{swap,undo,backup}
+mkdir -p $HOME/.vim/plugin
+ln -s $BASH_MINIMAL_DIR/.vimrc $HOME/.vimrc
+ln -s $BASH_MINIMAL_DIR/.vim/plugin/bash-minimal.vim $HOME/.vim/plugin/bash-minimal.vim
